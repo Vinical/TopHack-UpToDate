0cc7a9d4d3c46b18aa4f2aa8a3c0245b91de448d
Theodore Whitbeck <55822504+Vinical@users.noreply.github.com>
2020-03-06 10:19:16 -0800

    Add files via upload

---
+++ TopHack.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 06, 2020 at 06:17 PM
+-- Server version: 5.5.64-MariaDB
+-- PHP Version: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `TopHack`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Categorie`
+--
+
+CREATE TABLE IF NOT EXISTS `Categorie` (
+  `catID` int(11) NOT NULL,
+  `catDes` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `HackArt`
+--
+
+CREATE TABLE IF NOT EXISTS `HackArt` (
+  `hackID` int(11) NOT NULL,
+  `hackCat` varchar(255) DEFAULT NULL,
+  `hackLang` varchar(255) DEFAULT NULL,
+  `pubDate` date DEFAULT NULL,
+  `lasUpdated` date DEFAULT NULL,
+  `hackLikes` int(11) DEFAULT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `catID` int(11) DEFAULT NULL,
+  `Liked` bit(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `HackVid`
+--
+
+CREATE TABLE IF NOT EXISTS `HackVid` (
+  `vidID` int(11) NOT NULL,
+  `vidLang` varchar(255) DEFAULT NULL,
+  `vidPubDate` date DEFAULT NULL,
+  `lasUpdated` date DEFAULT NULL,
+  `vidLikes` int(11) DEFAULT NULL,
+  `userID` int(11) DEFAULT NULL,
+  `catID` int(11) DEFAULT NULL,
+  `Liked` bit(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE IF NOT EXISTS `Users` (
+  `userID` int(11) NOT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `userAge` int(11) DEFAULT NULL,
+  `userCity` varchar(255) DEFAULT NULL,
+  `userState` varchar(255) DEFAULT NULL,
+  `userCountry` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `View`
+--
+
+CREATE TABLE IF NOT EXISTS `View` (
+  `viewID` int(11) NOT NULL,
+  `viewTime` varchar(255) DEFAULT NULL,
+  `hackID` int(11) DEFAULT NULL,
+  `vidID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Categorie`
+--
+ALTER TABLE `Categorie`
+  ADD PRIMARY KEY (`catID`);
+
+--
+-- Indexes for table `HackArt`
+--
+ALTER TABLE `HackArt`
+  ADD PRIMARY KEY (`hackID`),
+  ADD KEY `catID` (`catID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `HackVid`
+--
+ALTER TABLE `HackVid`
+  ADD PRIMARY KEY (`vidID`),
+  ADD KEY `catID` (`catID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- Indexes for table `View`
+--
+ALTER TABLE `View`
+  ADD PRIMARY KEY (`viewID`),
+  ADD KEY `hackID` (`hackID`),
+  ADD KEY `vidID` (`vidID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `HackArt`
+--
+ALTER TABLE `HackArt`
+  ADD CONSTRAINT `HackArt_ibfk_1` FOREIGN KEY (`catID`) REFERENCES `Categorie` (`catID`),
+  ADD CONSTRAINT `HackArt_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `Users` (`userID`);
+
+--
+-- Constraints for table `HackVid`
+--
+ALTER TABLE `HackVid`
+  ADD CONSTRAINT `HackVid_ibfk_1` FOREIGN KEY (`catID`) REFERENCES `Categorie` (`catID`),
+  ADD CONSTRAINT `HackVid_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `Users` (`userID`);
+
+--
+-- Constraints for table `View`
+--
+ALTER TABLE `View`
+  ADD CONSTRAINT `View_ibfk_1` FOREIGN KEY (`hackID`) REFERENCES `HackArt` (`hackID`),
+  ADD CONSTRAINT `View_ibfk_2` FOREIGN KEY (`vidID`) REFERENCES `HackVid` (`vidID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ UpToDate.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 06, 2020 at 06:18 PM
+-- Server version: 5.5.64-MariaDB
+-- PHP Version: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `UpToDate`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Match`
+--
+
+CREATE TABLE IF NOT EXISTS `Match` (
+  `MatchID` int(11) NOT NULL,
+  `RestaurantID` int(11) DEFAULT NULL,
+  `MatchDate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `MatchLink`
+--
+
+CREATE TABLE IF NOT EXISTS `MatchLink` (
+  `MatchLinkID` int(11) NOT NULL,
+  `MatchID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Restaurant`
+--
+
+CREATE TABLE IF NOT EXISTS `Restaurant` (
+  `RestaurantID` int(11) NOT NULL,
+  `RestaurantLocation` varchar(255) DEFAULT NULL,
+  `RestaurantRating` int(11) DEFAULT NULL,
+  `HoursOpen` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE IF NOT EXISTS `Users` (
+  `UserID` int(11) NOT NULL,
+  `UserName` varchar(255) DEFAULT NULL,
+  `UserPhoneNumber` varchar(15) DEFAULT NULL,
+  `UserPassword` varchar(15) DEFAULT NULL,
+  `LinkedIn` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Match`
+--
+ALTER TABLE `Match`
+  ADD PRIMARY KEY (`MatchID`),
+  ADD KEY `RestaurantID` (`RestaurantID`);
+
+--
+-- Indexes for table `MatchLink`
+--
+ALTER TABLE `MatchLink`
+  ADD PRIMARY KEY (`MatchLinkID`),
+  ADD KEY `MatchID` (`MatchID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `Restaurant`
+--
+ALTER TABLE `Restaurant`
+  ADD PRIMARY KEY (`RestaurantID`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Match`
+--
+ALTER TABLE `Match`
+  ADD CONSTRAINT `Match_ibfk_1` FOREIGN KEY (`RestaurantID`) REFERENCES `Restaurant` (`RestaurantID`);
+
+--
+-- Constraints for table `MatchLink`
+--
+ALTER TABLE `MatchLink`
+  ADD CONSTRAINT `MatchLink_ibfk_1` FOREIGN KEY (`MatchID`) REFERENCES `Match` (`MatchID`),
+  ADD CONSTRAINT `MatchLink_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
